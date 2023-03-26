604e87610545ed744e362cdbe2cd33d74bdb8fa7
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-03-27 00:36:16 +0800

    menambahkan class SayaTubeVideo”

--- tpmodul6_1302210113/Program.cs
+++ tpmodul6_1302210113/Program.cs
@@ -4,10 +4,6 @@ using tpmodul6_1302210113;
 
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - [Yakup Asmaidy Atanggae_PRAKTIKAN]");
 
-        //test prekondisi
-        //video = new SayaTubeVideo(null);
-        //video = new SayaTubeVideo("Judul video dengan panjang lebih dari 100 karakter akan menghasilkan ContractException");
-
         for (int i = 0; i < 1000000000; i += 10000000)
         {
             video.IncreasePlayCount(10000000);
