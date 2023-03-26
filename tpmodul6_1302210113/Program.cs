b31992b42b875d5e7d5a5692ef0425dbad9cf179
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-03-27 00:35:29 +0800

    menambahkan class SayaTubeVideo”

--- tpmodul6_1302210113/Program.cs
+++ tpmodul6_1302210113/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-  
+using tpmodul6_1302210113;
+
+
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - [Yakup Asmaidy Atanggae_PRAKTIKAN]");
+
+        //test prekondisi
+        //video = new SayaTubeVideo(null);
+        //video = new SayaTubeVideo("Judul video dengan panjang lebih dari 100 karakter akan menghasilkan ContractException");
+
+        for (int i = 0; i < 1000000000; i += 10000000)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+        video.PrintVideoDetails();
